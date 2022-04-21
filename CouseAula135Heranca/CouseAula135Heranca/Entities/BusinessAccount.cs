8d00ff0c0afe44c809576115f25e824ef65e18f0
ïwellingtonmoura <wellingtonmoura@gmail.com>
2022-04-21 20:56:18 -0300

    Aula de Herança de Classe

---
+++ CouseAula135Heranca/CouseAula135Heranca/Entities/BusinessAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace CouseAula135Heranca.Entities
+{
+    public class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+        }
+
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) : base (number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+
+        }
+
+           
+        
+    }
+}
---
+++ CouseAula135Heranca/CouseAula135Heranca/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CouseAula135Heranca.Entities;
+
+namespace CouseAula135Heranca
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            BusinessAccount businessAccount = new BusinessAccount(116792, "Wellington", 1000.0, 10000.00);
+
+            Console.WriteLine(businessAccount.Balance);
+
+            //Nào posso modificar pois a propriedade é Protected
+            //businessAccount.Balance = 10000.00;
+        }
+    }
+}

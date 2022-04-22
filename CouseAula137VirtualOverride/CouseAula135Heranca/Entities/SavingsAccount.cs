3b6add5b62f061eb140bc1ad04981f6ba9616c28
ïwellingtonmoura <wellingtonmoura@gmail.com>
2022-04-21 22:10:53 -0300

    Aula de Sobreposição, Virtual, Override e Base

---
+++ CouseAula137VirtualOverride/CouseAula135Heranca/Entities/SavingsAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace CourseAula137VirtualOverride.Entities
+{
+    public class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate)
+
+            : base (number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+
+        public override void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+    }
+}
---
+++ CouseAula137VirtualOverride/CouseAula135Heranca/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CourseAula137VirtualOverride.Entities;
+
+namespace CouseAula135Heranca
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Account account1 = new Account(1001, "Alex", 500.00);
+            Account account2 = new SavingsAccount(1002, "Anna", 500.00, 0.001);
+
+            account1.Withdraw(10.0);
+            account2.Withdraw(10.0);
+
+            Console.WriteLine(account1.Balance);
+            Console.WriteLine(account2.Balance);
+        }
+    }
+}

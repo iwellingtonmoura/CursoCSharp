d070a846bbaec5783518ff785b8e4c70889a13c5
ïwellingtonmoura <wellingtonmoura@gmail.com>
2022-04-25 21:05:29 -0300

    Exercicio finalizado com tratamento de exeções

--- CouseAula152ExcecoesPersonalizadas/CouseAula152ExcecoesPersonalizadas/Entities/Exceptions/DomainException.cs
+++ CouseAula152ExcecoesPersonalizadas/CouseAula152ExcecoesPersonalizadas/Entities/Exceptions/DomainException.cs
@@ -3,7 +3,7 @@ namespace CouseAula152ExcecoesPersonalizadas.Entities.Exceptions
 {
     public class DomainException : ApplicationException
     {
-        public DomainException( string message) : base(message)
+        public DomainException( string message) : base(message )
         {
         }
     }

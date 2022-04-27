078f4825286400aaf19c7aedbdd97ffee2a137ca
ïwellingtonmoura <wellingtonmoura@gmail.com>
2022-04-26 21:18:32 -0300

    Utilização do bloco using para fechar arquivos de forma implicita

---
+++ CouseAula197BlocoUsing/CouseAula197BlocoUsing/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace CouseAula197BlocoUsing
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Ver como acesar diretorio no Mac
+            string path = @"c:\temp\file1.txt";
+
+            try
+            {
+                //Instanciando a classe FileStream
+
+                //using (FileStream fs = new FileStream(path, FileMode.Open))
+                //{
+                //    using (StreamReader sr = new StreamReader(fs))
+                //    {
+                //        while (!sr.EndOfStream)
+                //        {
+                //            string line = sr.ReadLine();
+                //            Console.WriteLine(line);
+                //        }
+                //    }
+                //}
+
+                //Trabalhando de forma mais enxuta usando a classe File.
+
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+
+
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("erro: " + ex.Message);
+            }
+
+
+        }
+    }
+}

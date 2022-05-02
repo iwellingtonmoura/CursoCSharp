0044d57617ca2bf3e6798a00d1785cb89382f578
ïwellingtonmoura <wellingtonmoura@gmail.com>
2022-05-01 21:38:22 -0300

    Exercicio de Fixação -  Interface, List, Injeção de Dependência - Exemplo Contratos

---
+++ CouseAula208ExercicioFixacao/CouseAula208ExercicioFixacao/Services/IOnlinePaymentService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace CouseAula208ExercicioFixacao.Services
+{
+    public interface IOnlinePaymentService
+    {
+        double PaymentFee(double amount);
+        double Interest(double amount, int months);
+    }
+}

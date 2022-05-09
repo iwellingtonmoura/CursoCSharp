67fcefba1f32951b6298b1aee9bc6c30e254935e
ïwellingtonmoura <wellingtonmoura@gmail.com>
2022-05-09 20:22:11 -0300

    Generics Part 1

---
+++ CouseAula213GernericsParte1/CouseAula213GernericsParte1/PrintService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace CouseAula213GernericsParte1
+{
+    public class PrintService
+    {
+        private int[] _values = new int[10];
+        private int _count = 0;
+
+        public PrintService()
+        {
+        }
+
+
+        public void addValue( int value)
+        {
+            if(_count == 10)
+            {
+                throw new InvalidOperationException(" PrintService is full!");
+            }
+
+            _values[_count] = value;
+            _count++; 
+        }
+
+        public int First()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException(" PrintService is empty!");
+            }
+
+            return _values[0];
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("[");
+
+            for (int i = 0; i < _count - 1; i++)
+            {
+                Console.Write(_values[i] + ",  ");
+            }
+            if(_count > 0)
+            {
+                Console.Write(_values[_count -1]);
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
---
+++ CouseAula213GernericsParte1/CouseAula213GernericsParte1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CouseAula213GernericsParte1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            PrintService printService = new PrintService();
+
+            Console.Write("How many values? :");
+            int n = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Add numbers ");
+                int x = int.Parse(Console.ReadLine());
+
+                printService.addValue(x);
+
+            }
+
+            printService.Print();
+
+            Console.Write("Fist: " + printService.First());
+            
+        }
+    }
+}
+ 

04d9a9c992e004ccdec6573b0ff1ee2ec4cbd2f4
ïwellingtonmoura <wellingtonmoura@gmail.com>
2022-05-16 21:52:57 -0300

    HashSet e SortedSet

---
+++ CouseAula216HashCodeEEquals/CouseAula216HashCodeEEquals/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CouseAula216HashCodeEEquals
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string a = "Wellington";
+            string b = "Pedro";
+
+            Console.WriteLine("Hash Wellington " + a.GetHashCode());
+            Console.WriteLine("Hash Pedro " + b.GetHashCode());
+        }
+    }
+}
---
+++ CouseAulaHashSetESortedSet/CouseAulaHashSetESortedSet/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CouseAulaHashSetESortedSet
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            SortedSet<int> a = new SortedSet<int>() { 2, 4, 5, 7, 9, 19 };
+            SortedSet<int> b = new SortedSet<int>() { 1, 3, 4, 8, 10, 9 };
+
+            SortedSet<int> c = new SortedSet<int>(a);
+
+            //Union
+            c.UnionWith(b);
+
+            //Intersection
+
+            SortedSet<int> d = new SortedSet<int>(a);
+            d.IntersectWith(b);
+
+
+            SortedSet<int> e = new SortedSet<int>(a);
+            e.ExceptWith(b);
+
+            PrintColletction(c);
+            PrintColletction(d);
+            PrintColletction(e);
+
+        }
+
+
+        static void PrintColletction<T>(IEnumerable<T> collection)
+        {
+            foreach(T obj in collection)
+            {
+                Console.Write(obj + " ");
+
+            }
+
+            Console.WriteLine();
+        }
+    }
+}

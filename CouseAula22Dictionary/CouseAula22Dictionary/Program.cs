b9c6c3330c45f7e27a4eb36b199a329f211952af
ïwellingtonmoura <wellingtonmoura@gmail.com>
2022-05-25 20:03:16 -0300

    Dictionary e SortedDictionary

---
+++ CouseAula22Dictionary/CouseAula22Dictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CouseAula22Dictionary
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            cookies["User"] = "Maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["phone"] = "99999999";
+
+            foreach(KeyValuePair<string, string> item in cookies)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+            
+        }
+    }
+}

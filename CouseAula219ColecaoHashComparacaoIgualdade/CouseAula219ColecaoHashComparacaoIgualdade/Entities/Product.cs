f98d6fc9cbc2a78d798553a02f3909b6e97745bf
ïwellingtonmoura <wellingtonmoura@gmail.com>
2022-05-24 21:03:13 -0300

    Aula de Coleção HashCode e Comparação por igualdade (Equals)

---
+++ CouseAula219ColecaoHashComparacaoIgualdade/CouseAula219ColecaoHashComparacaoIgualdade/Entities/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace CouseAula219ColecaoHashComparacaoIgualdade.Entities
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() + Price.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+
+            if(!(obj is Product))
+            {
+                return false;
+            }
+
+            //Downcast do objeto
+            Product other = obj as Product;
+
+          return ((Product)obj).Name.Equals(other.Name) && Price.Equals(other.Price);
+
+        }
+    }
+}

885be74ff4abcbd468a02d50dd8a2150ddb8e44a
ïwellingtonmoura <wellingtonmoura@gmail.com>
2022-05-26 11:49:54 -0300

    Extension methods

---
+++ CouseAula226ExtensionMethods/CouseAula226ExtensionMethods/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CouseAula226ExtensionMethods.Extensions;
+
+namespace CouseAula226ExtensionMethods
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            DateTime data = new DateTime(2018, 11, 10, 18, 45, 35);
+
+            Console.WriteLine(data.ElapsedTime());
+        }
+    }
+}

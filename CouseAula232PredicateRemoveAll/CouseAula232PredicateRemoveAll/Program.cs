cb70cea9bb9717633e3d4190c1401c0f3454caf9
ïwellingtonmoura <wellingtonmoura@gmail.com>
2022-05-31 20:00:03 -0300

    Aula 232 Predicate RemoveAll Lambda

---
+++ CouseAula232PredicateRemoveAll/CouseAula232PredicateRemoveAll/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using CouseAula232PredicateRemoveAll.Entities;
+
+namespace CouseAula232PredicateRemoveAll
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            list.RemoveAll(p => p.Price >= 100.00);
+
+            foreach(Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+
+        }
+    }
+}

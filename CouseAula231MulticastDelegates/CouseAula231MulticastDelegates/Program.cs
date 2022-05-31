a57f489c64089a7c760124336fd189d70ca22649
ïwellingtonmoura <wellingtonmoura@gmail.com>
2022-05-30 21:06:01 -0300

    Aula MultiCast - Delegate

---
+++ CouseAula231MulticastDelegates/CouseAula231MulticastDelegates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CouseAula231MulticastDelegates.Services;
+
+namespace CouseAula231MulticastDelegates
+{
+    delegate void BinaryNunericOpertiont(double n1, double n2);
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+
+            BinaryNunericOpertiont op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+
+            op.Invoke(a, b);
+
+
+          
+        }
+    }
+}
---
+++ CouseAula231MulticastDelegates/CouseAula231MulticastDelegates/Services/CalculationService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace CouseAula231MulticastDelegates.Services
+{
+    public class CalculationService
+    {
+
+        public static void ShowMax(double x, double y)
+        {
+            double max =  (x > y) ? x : y;
+            Console.WriteLine(max);
+        }
+
+        public static void ShowSum(double x, double y)
+        {
+            double max = x + y;
+            Console.WriteLine(max);
+        }
+
+
+
+    }
+}

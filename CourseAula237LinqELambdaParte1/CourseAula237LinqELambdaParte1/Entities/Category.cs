cb8b26c0a11ad71f94cb640c29a0584cb5b7955b
ïwellingtonmoura <wellingtonmoura@gmail.com>
2022-06-01 20:52:55 -0300

    Linq com notação similar ao SQL

---
+++ CourseAula237LinqELambdaParte1/CourseAula237LinqELambdaParte1/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace CourseAula237LinqELambdaParte1.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+    }
+}
---
+++ CouseAula240LinqNotacaoSimilarSQL/CouseAula240LinqNotacaoSimilarSQL/Entites/Category.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace CouseAula240LinqNotacaoSimilarSQL.Entites
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+    }
+}
